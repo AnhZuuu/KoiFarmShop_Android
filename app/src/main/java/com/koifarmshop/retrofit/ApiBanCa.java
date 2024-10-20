81662820a26ec1eb4d7372628a992e78df583839
Tien <thuytienln238@gmail.com>
2024-10-21 04:28:05 +0700

    Update ApiBanCa

--- app/src/main/java/com/koifarmshop/retrofit/ApiBanCa.java
+++ app/src/main/java/com/koifarmshop/retrofit/ApiBanCa.java
@@ -3,6 +3,7 @@ package com.koifarmshop.retrofit;
 import io.reactivex.rxjava3.core.Observable;
 import com.koifarmshop.model.FishKindModel;
 import com.koifarmshop.model.NewKoiModel;
+import com.koifarmshop.model.UserModel;
 
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -21,6 +22,14 @@ public interface ApiBanCa {
     Observable<NewKoiModel> getSingle(
             @Field("page") int page,
             @Field("loai") int loai
+    );
+    @POST("register.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangky(
+            @Field("email") String email,
+            @Field("password") String password,
+            @Field("name") String name,
+            @Field("phone") String phone
     );
 
 
